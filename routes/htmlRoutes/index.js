357ff125fecb9408640e0655dc17ea2ffee06829
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-04 06:15:59 -0500

    Update to export module router

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const router = require('express').Router();
+
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+module.exports = router
