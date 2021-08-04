491dbb886cad50a63950e827706e38b0262aff98
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-04 06:15:46 -0500

    Update to export module router

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,3 +1,16 @@
+const path = require('path');
+const router = require('express').Router();
+
 // GET /notes sould return the notes.html file
 
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+})
+
 // GET * should return the index.html file
+
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+})
+
+module.exports = router
