5491031401bcee01552263df779b7fd906aa3e43
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-08 10:20:47 -0500

    Simplify get, post, delete functionality for Heroku compatibility

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,43 +1,29 @@
 const router = require('express').Router();
-const notes = require("../../db/db.json");
-const fs = require('fs');
-const path = require('path');
+let notes = require("../../db/db.json");
 const { v4: uuidv4 } = require('uuid');
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// GET notes
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 })
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you, like uuid).
+// POST notes
 
 router.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     const newNote = req.body;
-    notes.push(newNote)
-    res.json(notes)
+    notes.push(newNote);
+    res.json(notes);
 })
 
-// DELETE 
+// DELETE notes
 
 router.delete('/notes/:id', (req, res) => {
     let noteId = req.params.id.toString(); 
-    fs.readFile(path.join(__dirname, "../../db/db.json"), "utf8", (err, data) => {
-        if (err) {
-            throw new Error("Failed to read file!");
-        }
-        let noteData = JSON.parse(data)
-        console.log('noteData has been parsed and appears as follows', noteData)
-        const modData = noteData.filter(notes => notes.id !== noteId);
-        console.log('modData has been defined successfully.')
-        fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(modData), (err, data) => {
-            if (err) {
-                throw new Error(err);
-            }
-            res.json(modData);
-        })
-    })
+    const modData = notes.filter(notes => notes.id !== noteId);
+    notes = modData
+    res.json(modData);
 })
 
 module.exports = router
