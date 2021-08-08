872e169ecc4d0156082702768f58e882d66caf63
rrcampbell-exe <campbell.ryan.r@gmail.com>
2021-08-08 06:42:43 -0500

    Add delete functionality

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const notes = require("../../db/db.json")
+const notes = require("../../db/db.json");
+const fs = require('fs');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 router.get('/notes', (req, res) => {
-    // console.log(uuidv4())
     res.json(notes);
 })
 
@@ -16,6 +17,27 @@ router.post('/notes', (req, res) => {
     const newNote = req.body;
     notes.push(newNote)
     res.json(notes)
+})
+
+// DELETE 
+
+router.delete('/notes/:id', (req, res) => {
+    let noteId = req.params.id.toString(); 
+    fs.readFile(path.join(__dirname, "../../db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            throw new Error("Failed to read file!");
+        }
+        let noteData = JSON.parse(data)
+        console.log('noteData has been parsed and appears as follows', noteData)
+        const modData = noteData.filter(notes => notes.id !== noteId);
+        console.log('modData has been defined successfully.')
+        fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(modData), (err, data) => {
+            if (err) {
+                throw new Error(err);
+            }
+            res.json(modData);
+        })
+    })
 })
 
 module.exports = router
